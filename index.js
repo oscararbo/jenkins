d42bb541acdedc104c862eace9de53bc6d2ec47c
oscararbo <oscararboleya@gmail.com>
2025-11-04 09:27:51 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,3 @@
+function saludar() {
+ alert("Esto es JavaScript ejecutándose.");
+}
---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+ echo "<h1>Ahora estás en PHP</h1>";
+ echo "<p>La fecha y hora del servidor es: " . date("d/m/Y H:i:s") . "</p>";
+?>
